9970669d98e7e044ebfc8e3d7f5eb73a1df84c98
NaumanRecat <naumantariq2977@gmail.com>
2021-11-05 02:45:15 -0700

    splash

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,11 +1,41 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
-function Splash () {
+const Txt = () => {
     return(
-        <View>
-            <Text> kinza </Text>
+        <View style={styles.fnparentview}> 
+        <Image style={styles.imagesty} source={require('../assests/logo.jpg')}/>
+        <Text style={styles.Text1}> wallpaper 71 </Text>
+        <Text style={styles.Text2}> 8K,4K, FULL HD,HD WALLPAPER</Text>
         </View>
     )
 }
+
+function Splash () {
+    return(
+        <View style={styles.parentView}>
+            <Txt/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    parentView:{
+        flex:1, backgroundColor: '#333333', justifyContent:'center'
+    },
+    fnparentview:{
+        height: 300, justifyContent:'center', alignItems:'center'
+    },
+    imagesty:{
+        height: 130, width: 90, backgroundColor:'#faf'
+    },
+    Text1:{
+        fontSize:25, fontWeight:'bold', color:'#FFFFFF'
+    },
+    Text2:{
+        fontSize:14, color:'#FFFFFF'
+    }
+});
+
+
 export default Splash;
