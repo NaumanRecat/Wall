0ecd146b583aa0c40bf3c45462ac61d0e92bfd37
NaumanRecat <naumantariq2977@gmail.com>
2021-11-06 00:24:07 -0700

    done

---
+++ src/screens/categories/Category.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {widthPercentageToDP as w, heightPercentageToDP as h} from 'react-native-responsive-screen';
+
+function Category () {
+    return(
+        <View>
+            <Text>
+                test
+            </Text>
+        </View>
+    )
+}
+
+export default Category;
--- src/screens/navigator/ScreenNavigator.js
+++ src/screens/navigator/ScreenNavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Splash from '../Splash';
+import Category from '../categories/Category';
 
 
 
@@ -12,6 +13,7 @@ export const ScreenNavigator =()=>  {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen name="Category" component={Category}  options={{headerShown:false}} />
       <Stack.Screen name="Splash" component={Splash}  options={{headerShown:false}} />
       </Stack.Navigator>
     </NavigationContainer>
