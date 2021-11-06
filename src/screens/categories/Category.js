b651665310cd7bdbc400c030ef02c88855e4d968
NaumanRecat <naumantariq2977@gmail.com>
2021-11-06 01:13:19 -0700

    flatlist

--- src/screens/categories/Category.js
+++ src/screens/categories/Category.js
@@ -1,13 +1,46 @@
 import React from "react";
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, FlatList, useState} from 'react-native';
 import {widthPercentageToDP as w, heightPercentageToDP as h} from 'react-native-responsive-screen';
 
+const DATA = [
+    {
+      title: 'First Item',
+    },
+    {
+      title: 'Second Item',
+    },
+    {
+      title: 'Third Item',
+    },
+    
+  ];
+
+  const renderItem = ({item}) => (
+      <View style={{
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+      }}>
+          <Text style={{
+              fontSize: 32,
+          }}>
+              {item.title}
+          </Text>
+      </View>
+  )
+
 function Category () {
+
     return(
-        <View>
-            <Text>
-                test
-            </Text>
+        <View style={{
+            flex: 1,
+        }}>
+        <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.title}
+      />
         </View>
     )
 }
